4491369b7caaebece7f19a12332863f51f33485f
DanielaReinhardt <reinhardtd@telekom.de>
2022-02-24 11:19:50 +0100

    Learning loops

---
+++ Loops.java
@@ -0,0 +1,12 @@
+class Loops {
+    public static void main(String[] args) {
+      String [] month = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+       
+    
+    for(int i=0; i < month.length; i++){
+
+   
+    System.out.println("The month is " + month[i]);
+}
+}
+}
